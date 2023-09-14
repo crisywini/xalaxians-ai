1970e4234a4ae69d580a275d22958d5c00b3f935
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-09-14 14:32:39 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A systems that allows to answer questions about the xalaxians',
+    author='Cristian Sánchez Pineda',
+    license='MIT',
+)
